163386b3b3f006488d815f29b66d323bb0c40ec1
Javascript_Developer <ah1502651@gmail.com>
2025-07-10 12:29:53 +0500

    change in cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -24,7 +24,8 @@ const CursorParticles = () => {
         this.size = Math.random() * 7 + 1;
         this.speedX = Math.random() * 1.2 - 0.6; // Slower movement
         this.speedY = Math.random() * 1.2 - 0.6; 
-        this.color = `hsl(${hue.current}, 100%, 50%)`;
+        // this.color = `hsl(${hue.current}, 100%, 50%)`;
+        this.color = '#ffffff'
         this.life = 180; // Lasts longer
       }
       update() {
